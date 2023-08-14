827fecee2d848770ca9865dd23879a1e97abd06d
YoungDuck1592 <tjdudejr1919@gmail.com>
2023-08-14 20:15:55 +0900

    feature/ProfilePageUI(0814)

--- ThreadApp/Model/Thread.swift
+++ ThreadApp/Model/Thread.swift
@@ -10,7 +10,7 @@ import Foundation
 struct Thread {
     var title: String
     var createdAt: Date
-    var commentCount: Int
+    var content: Int
     var photoData: Data?
     let authorProfile: Profile
     var comments: [Comment]?
