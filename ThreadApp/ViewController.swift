200eceb5fef5d633e1aa8b76b2556803b2457fbb
cheshire0105 <cheshire0105@gmail.com>
2023-08-14 11:22:16 +0900

    1

---
+++ ThreadApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ThreadApp
+//
+//  Created by cheshire on 2023/08/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
